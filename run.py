8e2824ac4866f32fba46d3d9146f29bdfce30db6
Seafzz <163156309+Seafzz@users.noreply.github.com>
2024-06-24 15:36:33 +0000

    add food placement in the game

--- run.py
+++ run.py
@@ -53,3 +53,6 @@ if played_before == 'no':
         [snk_y, snk_x -1],
         [snk_y, snk_x -2]
     ]
+#Create the food
+food = sh[sh//2, sw//2]
+w.addch(int(food[0])), int(food[1]), curses.ACS_PI)
