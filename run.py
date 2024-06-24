fbde76cd5fdf45cdf9a05eed142e7a9484870cfc
Seafzz <163156309+Seafzz@users.noreply.github.com>
2024-06-24 15:22:14 +0000

    added a function to ask for the players name, if they have played the game and with a loop decided what they would get as a response depending on their awnser

--- run.py
+++ run.py
@@ -1,3 +1,28 @@
 import random
 import curses
 import _curses
+
+#ASk the user for the user name
+
+player_name = input ("What is your name? ")
+
+#Have you played snake before?
+while true:
+    played_before = input ("Have you played this game before? (Yes/No)").lower()
+    if played_before['y', 'N']:
+        break
+    else:
+        print("Invalid input. Please enter 'Y' or 'N' ")
+
+#If the player has played the game before skip instructions
+
+if played_before == 'no':
+    while true:
+        controll_the_snake = input("Use the arrow keys to control the snake\
+        You can hold down the arrows to make the snake go faster\
+        Do not let the snake touch the sides of the game area or eat itself!\
+        Press enter to continue. ")
+    if controll_the_snake == '':
+        break
+    else:
+        print("Invalid input. Press Enter to continue")
